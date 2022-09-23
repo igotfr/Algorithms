e62433d40320a6c9b3314f8bfac3e7beed28df56
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2022-09-23 20:26:36 -0300

    Create palindrome_number.rs

---
+++ Palindrome_Number/palindrome_number.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn is_palindrome(mut x: i32) -> bool {
+        // Iterative
+        if x < 0 || (x % 10 == 0 && x != 0) {
+            return false
+        }
+
+        let mut reverted_number = 0;
+        while x > reverted_number {
+            reverted_number *= 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reverted_number || x == reverted_number / 10
+    }
+
+    /*pub fn is_palindrome(x: i32) -> bool {
+        // Functional
+        return x.to_string().chars().rev().collect::<String>() == x.to_string()
+    }*/
+}
