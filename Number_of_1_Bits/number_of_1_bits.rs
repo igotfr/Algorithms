28e692d28d921cc095a3885c2c5c42ba3195efdd
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2022-09-23 22:16:15 -0300

    Create number_of_1_bits.rs

---
+++ Number_of_1_Bits/number_of_1_bits.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    //pub fn hammingWeight(mut n: u32) -> i32 {
+        // Iterative
+        /*let mut ans = 0;
+
+        while n != 0 {
+            ans += n & 1;
+            n >>= 1;
+        }
+
+        return ans as i32*/
+    //}
+
+    pub fn hammingWeight(n: u32) -> i32 {
+        // Functional
+        return n.count_ones() as i32
+        //return format!("{:b}", n).chars().filter(|x: &char| -> bool { x == &'1' }).count() as i32
+    }
+}
