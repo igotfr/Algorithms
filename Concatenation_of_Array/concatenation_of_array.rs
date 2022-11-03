f422fa5fdcc5046ac0d7e2557f14e5d8c0e49dc2
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2022-11-03 00:43:06 -0300

    Create concatenation_of_array.rs

---
+++ Concatenation_of_Array/concatenation_of_array.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
+        // Functional 1
+        //return [&*nums, &*nums].concat()
+
+        // Functional 2
+        return nums.repeat(2)
+    }
+
+    /*pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
+        // Iterative 1
+        /*let n = nums.len();
+        for i in 0..n {
+            nums.push(nums[i]);
+        }*/
+
+        // Functional 2
+        nums.extend(nums.clone());
+
+        // Iterative 1, Functinal 2
+        return nums
+    }*/
+}
